3cd49ac5b2952748e81032910d4550ba1eaf88d0
zhouselena <selena.y.zhou.26@dartmouth.edu>
2022-11-06 18:48:07 -0500

    first problem- binary search, initial solution, AC

---
+++ learningplan_binarysearch1/src/learningplan_binarysearch1/binarysearch_704.java
@@ -0,0 +1,33 @@
+package learningplan_binarysearch1;
+
+public class binarysearch_704 {
+	
+	public static int search(int[] nums, int target) {
+        
+		int left = 0;
+		int right = nums.length - 1;
+		
+		while (left <= right) {
+			
+			int midpoint = (right + left)/2;
+			
+			if (nums[midpoint] == target) return midpoint;
+			else if (nums[midpoint] > target) right = midpoint - 1;
+			else if (nums[midpoint] < target) left = midpoint + 1;
+			
+		}
+		
+		return -1;
+				
+    }
+
+	public static void main(String[] args) {
+		
+		int[] nums = {1, 2, 3, 4, 5};
+		int target = 5;
+		
+		System.out.println(search(nums,target));
+
+	}
+
+}
