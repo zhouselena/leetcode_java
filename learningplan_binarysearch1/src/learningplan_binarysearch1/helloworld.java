a85cb33a04a2cc0b642696025a00f18d8f8e9690
zhouselena <selena.y.zhou.26@dartmouth.edu>
2022-11-06 18:21:38 -0500

    removal of module-info, test if code works

---
+++ learningplan_binarysearch1/src/learningplan_binarysearch1/helloworld.java
@@ -0,0 +1,11 @@
+package learningplan_binarysearch1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
--- learningplan_binarysearch1/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author selenazhou
- *
- */
-module learningplan_binarysearch1 {
-}
