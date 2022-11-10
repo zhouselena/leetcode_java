913a81a785b0273e2f678c690b83e6dc3c23272e
zhouselena <selena.y.zhou.26@dartmouth.edu>
2022-11-10 11:30:52 -0500

    linear solution, slow but AC's

--- learningplan_binarysearch1/src/learningplan_binarysearch1/PeakIndexInMountainArray_852.java
+++ learningplan_binarysearch1/src/learningplan_binarysearch1/PeakIndexInMountainArray_852.java
@@ -1,9 +1,28 @@
 package learningplan_binarysearch1;
 
 public class PeakIndexInMountainArray_852 {
+	
+	public static int peakIndexInMountainArray(int[] arr) {
+		
+		int peak = -1;
+		
+		for (int i = 1; i < arr.length-1; i++) {
+			
+			if ((arr[i-1] < arr[i]) && (arr[i+1] < arr[i])) {
+				peak = i;
+				break;
+			}
+			
+		}
+		
+		return peak;
+        
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] mountain = {0,2,1,0};
+		System.out.println(peakIndexInMountainArray(mountain));
 
 	}
 
