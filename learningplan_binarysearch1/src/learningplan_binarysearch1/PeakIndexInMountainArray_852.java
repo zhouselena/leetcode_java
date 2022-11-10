fe8289c94e21678733038e7db9ae3ba7692393d9
zhouselena <selena.y.zhou.26@dartmouth.edu>
2022-11-10 11:40:25 -0500

    binary search method, used help, runtime o(log(n)), AC

--- learningplan_binarysearch1/src/learningplan_binarysearch1/PeakIndexInMountainArray_852.java
+++ learningplan_binarysearch1/src/learningplan_binarysearch1/PeakIndexInMountainArray_852.java
@@ -4,18 +4,28 @@ public class PeakIndexInMountainArray_852 {
 	
 	public static int peakIndexInMountainArray(int[] arr) {
 		
-		int peak = -1;
+		int low = 1;
+		int high = arr.length-1;
+		int mid;
 		
-		for (int i = 1; i < arr.length-1; i++) {
+		while (low <= high) {
 			
-			if ((arr[i-1] < arr[i]) && (arr[i+1] < arr[i])) {
-				peak = i;
-				break;
+			mid = (low + high) / 2;
+			
+			if ((arr[mid-1] < arr[mid]) && (arr[mid+1] < arr[mid])){ // if mid is the peak
+				return mid;
+			}
+			
+			else if ((arr[mid-1] < arr[mid]) && (arr[mid] < arr[mid+1])) { // otherwise if still climbing up mountain
+				low = mid+1;
+			}
+			else { // otherwise peak is on left of mid, passed peak
+				high = mid-1;
 			}
 			
 		}
 		
-		return peak;
+		return -1; // didn't find peak
         
     }
 
